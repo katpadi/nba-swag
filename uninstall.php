18365dd0e99b12c43f5fc2bc5da9ca57a1b25d31
Kat Padilla <katpadillaaa@gmail.com>
2014-07-16 20:00:48 +0800

    Just renamed the TODO in uninstall

--- uninstall.php
+++ uninstall.php
@@ -2,11 +2,11 @@
 /**
  * Fired when the plugin is uninstalled.
  *
- * @package   Plugin_Name
- * @author    Your Name <email@example.com>
+ * @package   Nba_Swag
+ * @author    Kat Padi <hello@katpadi.ph>
  * @license   GPL-2.0+
  * @link      http://example.com
- * @copyright 2014 Your Name or Company Name
+ * @copyright 2014 Kat Padilla
  */
 
 // If uninstall not called from WordPress, then exit
